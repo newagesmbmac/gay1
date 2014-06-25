df2d36fa41e6587f32ff75e7a358f65c426d0c9a
NewageSMB <newagesmb@NewageSMBs-Mac-Pro.local>
2014-06-25 23:02:36 +0530

    Initial Commit

---
+++ Gay1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gay1
+//
+//  Created by NewageSMB on 6/25/14.
+//  Copyright (c) 2014 NewageSMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gay1Tests/Gay1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Gay1Tests.m
+//  Gay1Tests
+//
+//  Created by NewageSMB on 6/25/14.
+//  Copyright (c) 2014 NewageSMB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Gay1Tests : XCTestCase
+
+@end
+
+@implementation Gay1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
